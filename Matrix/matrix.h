b91e155f044e5331ff8474280451b77f7ff357ba
jpradas1 <jpradas@unal.edu.co>
2022-02-18 13:21:38 -0500

    matrix library implementation

---
+++ Matrix/matrix.h
@@ -0,0 +1,17 @@
+#ifndef _MATRIX_H_
+#define _MATRIX_H_
+
+#include <iostream>
+
+template <class T>
+class nmMatrixN{
+public:
+
+
+private:
+    T *matrix1DData;
+    int nRows, nCols, nElements;
+
+};
+
+#endif
